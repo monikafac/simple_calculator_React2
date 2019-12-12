89eb9d4dedbc2fd8b5a93d6c905b4c7636401075
Monika Fac <monika.fac@gmail.com>
2019-12-12 11:31:47 +0100

    Updates

---
+++ precache-manifest.a5c3d4ebee4ad7c84264b96341bc61c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e428e3e5c827b8635f4a05d29268373f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c43a31dea77f220b7b9",
+    "url": "/static/css/main.90f607c1.chunk.css"
+  },
+  {
+    "revision": "d8c9d0feabbb1dedc953",
+    "url": "/static/js/2.a096fb46.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a096fb46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c43a31dea77f220b7b9",
+    "url": "/static/js/main.7e419362.chunk.js"
+  },
+  {
+    "revision": "f8c7fe37d5f71a08268d",
+    "url": "/static/js/runtime-main.738bfeb5.js"
+  }
+]);
